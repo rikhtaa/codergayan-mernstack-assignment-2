468a871a1cb65f17e79e810d797f164e518c81cf
Rekhta Menahil <rekhta674@gmail.com>
2025-07-30 02:27:58 +0500

    add typeorm

---
+++ src/entity/Admin.ts
@@ -0,0 +1,18 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+@Entity({ name: 'admin' })
+export class Admin {
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column('varchar', { length: 100 })
+    firstName!: string
+
+    @Column('varchar', { length: 100, unique: true })
+    email!: string
+
+    @Column('varchar', { length: 255 })
+    password!: string
+
+    @Column()
+    role!: number
+}
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,32 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import logger from './config/logger'
+import { AppDataSource } from './config/Data_Source'
 
-const app = express()
 dotenv.config()
+const app = express()
 
 app.post('/', (req, res) => {
     res.send('hello')
 })
-const port = process.env.PORT || 5501
-app.listen(port, () => {
-    console.log('server is listening on 5501')
-    logger.info('Server started successfully')
-})
+const startServer = async () => {
+    const port = process.env.PORT || 5501
+    try {
+        await AppDataSource.initialize()
+        logger.info('DB connected successfully')
+        app.listen(port, () => {
+            logger.info('Server started successfully')
+        })
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            logger.error(error.message)
+        } else {
+            logger.error('AN unknown error occured')
+        }
+
+        setTimeout(() => {
+            process.exit(1)
+        }, 1000)
+    }
+}
+void startServer()
