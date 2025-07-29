8979d4d3f51cec0c9ab663e928dd6f27bf90b4c3
Rekhta Menahil <rekhta674@gmail.com>
2025-07-29 14:08:48 +0500

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+// @ts-nocheck
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config({
+  extends: [
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    
+  ],
+  ignores: ['dist', 'node_modules'],
+  files: ['**/*.ts'],
+  languageOptions: {
+    parserOptions: {
+      project: './tsconfig.json',
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+   rules: {
+      'no-unreachable': 'error', 
+    },
+});
