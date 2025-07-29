8ac0789b747af4773b399305bd5ba2e00951d675
Rekhta Menahil <rekhta674@gmail.com>
2025-07-29 16:23:09 +0500

    add logger

---
+++ src/config/logger.ts
@@ -0,0 +1,25 @@
+import winston from 'winston'
+const logger = winston.createLogger({
+    level: 'info',
+    defaultMeta: { service: 'auth-service' },
+    transports: [
+        new winston.transports.File({
+            level: 'error',
+            dirname: 'logs',
+            filename: 'error.log',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+        }),
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+        }),
+    ],
+})
+console.log('logger')
+
+export default logger
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import logger from './config/logger'
+
 const app = express()
 dotenv.config()
 
@@ -9,4 +11,5 @@ app.post('/', (req, res) => {
 const port = process.env.PORT || 5501
 app.listen(port, () => {
     console.log('server is listening on 5501')
+    logger.info('Server started successfully')
 })
