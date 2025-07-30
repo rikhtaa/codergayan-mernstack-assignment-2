9d9d8e9746f87adfa02cfaec457af7f179cafbfc
Rekhta Menahil <rekhta674@gmail.com>
2025-07-30 16:43:40 +0500

    add server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,14 @@ import express from 'express'
 import dotenv from 'dotenv'
 import logger from './config/logger'
 import { AppDataSource } from './config/Data_Source'
-import { Admin } from './entity/Admin'
-import bcrypt from 'bcryptjs'
-import manager from './routes/manager'
 dotenv.config()
 export const app = express()
 
-app.use('/managers', manager)
 app.use(express.json())
 const startServer = async () => {
     const port = process.env.PORT || 5501
     try {
         await AppDataSource.initialize()
-        await createAdmin()
         logger.info('DB connected successfully')
         app.listen(port, () => {
             logger.info('Server started successfully')
@@ -32,28 +27,3 @@ const startServer = async () => {
     }
 }
 void startServer()
-
-const createAdmin = async () => {
-    try {
-        const adminRepository = AppDataSource.getRepository(Admin)
-        const checkAdmin = await adminRepository.findOne({
-            where: { role: 'admin' },
-        })
-        const salt = 10
-        const hashedPassword = await bcrypt.hash(process.env.password!, salt)
-        const userAdmin = {
-            firstName: process.env.firstName,
-            email: process.env.email,
-            password: hashedPassword,
-            role: process.env.role,
-        }
-        if (!checkAdmin) {
-            await adminRepository.save(userAdmin)
-            logger.info('Admin has been created')
-        } else {
-            logger.info('Admin already exists')
-        }
-    } catch (error) {
-        logger.error('Failed to create admin:', error)
-    }
-}
