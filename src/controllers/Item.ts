8a549cbac09364d63797d52bef246c837c9ac234
Rekhta Menahil <rekhta674@gmail.com>
2025-07-31 20:01:47 +0500

    add getall, delete and update item endpoint

--- src/controllers/Item.ts
+++ src/controllers/Item.ts
@@ -28,3 +28,115 @@ export const createItem = async (
         return
     }
 }
+
+export const getItemById = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    try {
+        const id = Number(req.params.id)
+        if (isNaN(id)) {
+            res.status(400).json({ message: 'invalid item id' })
+        }
+        const itemRepository = AppDataSource.getRepository(Item)
+        const item = await itemRepository.findOneBy({ id })
+
+        if (!item) {
+            return res
+                .status(401)
+                .json({ message: `No item found with this id ${id}` })
+        }
+        return res.status(201).json(item)
+    } catch (err) {
+        next(err)
+        return
+    }
+}
+
+export const deleteItemById = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    try {
+        const id = Number(req.params.id)
+        if (isNaN(id)) {
+            res.status(400).json({ message: 'invalid item id' })
+        }
+        const itemRepository = AppDataSource.getRepository(Item)
+        const item = await itemRepository.delete({ id })
+
+        if (!item) {
+            return res
+                .status(401)
+                .json({ message: `No item found with this id ${id}` })
+        }
+        return res
+            .status(201)
+            .json({ message: `item with this id ${id} has been deleted.` })
+    } catch (err) {
+        next(err)
+        return
+    }
+}
+
+export const updateItemById = async (
+    req: Request<{ id: string }, object, itemData>,
+    res: Response,
+    next: NextFunction,
+) => {
+    try {
+        const id = Number(req.params.id)
+        if (isNaN(id)) {
+            res.status(400).json({ message: 'invalid item id' })
+        }
+        const { price, productId } = req.body
+        const productRepository = AppDataSource.getRepository(Product)
+        const product = await productRepository.findOneBy({ id: productId })
+
+        if (!product) {
+            res.status(400).json({ message: 'product not found' })
+            return
+        }
+
+        const itemRepository = AppDataSource.getRepository(Item)
+        const result = await itemRepository.update(id, {
+            price,
+            product,
+        })
+
+        if (result.affected === 0) {
+            return res
+                .status(404)
+                .json({ message: `No item found with this id ${id}` })
+        }
+
+        const updatedItem = await itemRepository.findOne({
+            where: { id },
+            relations: ['product'],
+        })
+
+        return res.status(200).json({
+            message: `item with this id ${id} has been updated.`,
+            updatedItem,
+        })
+    } catch (err) {
+        next(err)
+        return
+    }
+}
+export const getAllItems = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    try {
+        const itemRepository = AppDataSource.getRepository(Item)
+        const allItems = await itemRepository.find()
+        return res.status(200).json({ message: 'All Items', allItems })
+    } catch (err) {
+        next(err)
+        return
+    }
+}
--- src/routes/item.ts
+++ src/routes/item.ts
@@ -1,5 +1,15 @@
 import express from 'express'
-import { createItem } from '../controllers/item'
+import {
+    createItem,
+    deleteItemById,
+    getAllItems,
+    getItemById,
+    updateItemById,
+} from '../controllers/Item'
 export const itemRouter = express()
 
 itemRouter.post('/', createItem)
+itemRouter.get('/:id', getItemById)
+itemRouter.delete('/:id', deleteItemById)
+itemRouter.patch('/:id', updateItemById)
+itemRouter.get('/', getAllItems)
