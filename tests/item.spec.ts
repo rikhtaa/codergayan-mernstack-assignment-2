8c386d702689fb60a93726f69875197c7a42f3b3
Rekhta Menahil <rekhta674@gmail.com>
2025-08-01 16:51:27 +0500

    add test for item post endpoint

--- tests/item.spec.ts
+++ tests/item.spec.ts
@@ -1,5 +1,8 @@
 import { DataSource } from 'typeorm'
 import { AppDataSource } from '../src/config/Data_Source'
+import { app } from '../src/server'
+import request from 'supertest'
+
 describe("Item Routes", ()=>{
   let connection: DataSource
     beforeAll(async () => {
@@ -16,7 +19,16 @@ describe("Item Routes", ()=>{
     })
     describe('POST /Item', () => {
     describe('given all fields', () => {
-       
+        it("should retrun 201 statusCode", async()=>{
+
+                 await request(app).post('/product').send({ name: "codebite1", description: "ed tech1" });
+                    const itemData = {
+                   price: 400,
+                   productId: 1 
+                  }
+                  const response = await request(app).post('/item').send(itemData)
+                  expect(response.statusCode).toBe(201)
+               })
     })
 })
 })
