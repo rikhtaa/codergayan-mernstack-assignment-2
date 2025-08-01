8aa9bfb53129870249dac5c54a00c97cdfe5b9a5
Rekhta Menahil <rekhta674@gmail.com>
2025-08-01 16:08:58 +0500

    add environment specific config setup for dev and test

--- src/config/Data_Source.ts
+++ src/config/Data_Source.ts
@@ -1,14 +1,14 @@
 import { DataSource } from 'typeorm'
 import 'reflect-metadata'
-import { config } from 'dotenv'
-config()
+import { Config } from '.'
+
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: String(process.env.DB_PASSWORD),
-    database: process.env.DB_NAME,
+    host: Config.DB_HOST,
+    port: Number(Config.DB_PORT),
+    username: Config.DB_USERNAME,
+    password: String(Config.DB_PASSWORD),
+    database: Config.DB_NAME,
     synchronize: false,
     logging: false,
     entities: ['src/entity/*.{ts,js}'],
